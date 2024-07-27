c554b0167f7e6d35b1f1c7efd5459f329beb4197
ChadGotei <memerdank199@gmail.com>
2024-07-27 11:14:16 +0530

    Main commit

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer className="flex flex-col md:flex-row justify-center items-center py-4 bg-gray-800 text-white z-10">
             <div className="flex gap-6">
-                <a href="www.linkedin.com/in/allin1-media" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/allin1-media" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin className="w-6 h-6 hover:text-gray-400" />
                 </a>
                 <a href="https://www.instagram.com/allin1mediain?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D" target="_blank" rel="noopener noreferrer">
